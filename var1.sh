40b72245f32c622868399641b40104936936b791
DiegoRyan0101 <181578930+DiegoRyan0101@users.noreply.github.com>
2025-06-29 16:25:31 -0300

    Update var1.sh

--- var1.sh
+++ var1.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-
+#Declarando uma variável em shell e chamando ela 
+#Para chamar uma variável você usa o sinal de cifrão antes do nome da variável
+#Para declar uma variável você da um nome e o sinal de igual e aspas tudo junto
+#E para exibir o conteúdo da variável basta dar um echo e o nome da variável
 VARIAVEL="UM DOIS TRÊS"
 echo $VARIAVEL
 echo
