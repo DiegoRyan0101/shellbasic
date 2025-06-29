77a1fa94363e2df6a57f54f6b8ad6e0bb30c4948
DiegoRyan0101 <181578930+DiegoRyan0101@users.noreply.github.com>
2025-06-29 16:27:14 -0300

    Update var2.sh

--- var2.sh
+++ var2.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-
+#Você pode declarar variáveis com valores, tipo comandos sendo declarados(automatização) ex abaixo
 HOJE=$(date)
 echo "hoje é: $HOJE"
 echo
+#O comando unset ele exclui o valor de uma variável
 unset HOJE
 echo $HOJE
