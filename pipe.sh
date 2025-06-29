a27b3200609ecd2138551bc99e584391fe92377c
DiegoRyan0101 <181578930+DiegoRyan0101@users.noreply.github.com>
2025-06-29 17:09:38 -0300

    Update pipe.sh

--- pipe.sh
+++ pipe.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
 cat /etc/passwd | grep root | cut -c1-10
+
+#O pipe é a barra em reta | 
+# o pipe permite que você execute mais de um comando em um único comando se parando os com o pipe
