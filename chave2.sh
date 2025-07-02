245c0114a38017e4b244fe58a844bb352fafe815
DiegoRyan0101 <181578930+DiegoRyan0101@users.noreply.github.com>
2025-07-02 16:01:23 -0300

    Add files via upload

---
+++ chave2.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#Use o 0(zero), para desligar as opções
+#e Use o 1(um) para ligar
+
+USAR_CORES=0	 #Mostra cores nas mensagens?
+CENTRALIZAR=0	#centralizar a mensagem na tela?
+SOAR_BIPE=0 	#soar um bipe ao mostrar a mensagem?
+CONFIRMAR=0	#pedir confirmação antes de mostrar?
+MOSTRA_E_SAI=0	#sair do programa após mostrar?
